dd76a98bd15e06045b8305aa4a7a7a69809894b6
heyongxian <1064042411@qq.com>
2018-10-31 16:43:13 +0800

    commit

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { CSSTransition } from 'react-transition-group';
+import   * as actionCreators from './store/actionCreator';
 import {
     HeaderWrapper, 
     Logo,
@@ -12,69 +13,59 @@ import {
     Button
 } from './style';
 
-class Header extends Component {
-
-    render() {
-        return (
-            <HeaderWrapper>
-                <Logo/>
-                <Nav>
-                    <NavItem className='left active'>首页</NavItem>
-                    <NavItem className='left'>下载App</NavItem>
-                    <NavItem className='right'>登陆</NavItem>
-                    <NavItem className='right'>
-                        <i className="iconfont">&#xe636;</i>
-                    </NavItem>
-                    <SearchWrapper>
-                        <CSSTransition 
-                            in = {this.props.focused}
-                            timeout={200}
-                            classNames= "slide"
-                        >
-                            <NavSearch
-                                className = {this.props.focused ? 'focused' : ''}
-                                onFocus = {this.props.handleInputFocus}
-                                onBlur = {this.props.handleInputBlur}
-                            ></NavSearch>
-                        </CSSTransition>
-                        <i className = {this.props.focused ? 'focused iconfont' : 'iconfont'}>
-                            &#xe60b;
-                        </i>
-                      
-                    </SearchWrapper>
-                </Nav>
-                <Addition>
-                    <Button className='reg'>注册</Button>
-                    <Button className='writting'>
-                    <i className="iconfont">&#xe678;</i>
-                        写文章
-                    </Button>
-                </Addition>
-            </HeaderWrapper>
-        )
-    }
-
+const Header = (props) => {
+    return(
+        <HeaderWrapper>
+            <Logo/>
+            <Nav>
+                <NavItem className='left active'>首页</NavItem>
+                <NavItem className='left'>下载App</NavItem>
+                <NavItem className='right'>登陆</NavItem>
+                <NavItem className='right'>
+                    <i className="iconfont">&#xe636;</i>
+                </NavItem>
+                <SearchWrapper>
+                    <CSSTransition 
+                        in = {props.focused}
+                        timeout={200}
+                        classNames= "slide"
+                    >
+                        <NavSearch
+                            className = {props.focused ? 'focused' : ''}
+                            onFocus = {props.handleInputFocus}
+                            onBlur = {props.handleInputBlur}
+                        ></NavSearch>
+                    </CSSTransition>
+                    <i className = {props.focused ? 'focused iconfont' : 'iconfont'}>
+                        &#xe60b;
+                    </i>
+                </SearchWrapper>
+            </Nav>
+        <Addition>
+            <Button className='reg'>注册</Button>
+            <Button className='writting'>
+            <i className="iconfont">&#xe678;</i>
+                写文章
+            </Button>
+        </Addition>
+    </HeaderWrapper>
+    );
 }
+
 
 const mapStateToProps = (state) => {
     return {
-        focused: state.focused
+        focused: state.header.focused
     }
 }
 
 const mapDispathToProps = (dispatch) =>{
     return{
         handleInputFocus(){
-            const action = {
-                type: 'search_focus'
-            };
-            dispatch(action);
+            dispatch(actionCreators.searchFocus());
         },
         handleInputBlur(){
-            const action = {
-                type: 'search_blur'
-            };
-            dispatch(action);
+            dispatch(actionCreators.searchBlur());
         }
     }
 }
